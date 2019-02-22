ae32c0a7b64b9964e07c26415174f6ffa1c18f5f
eserhin <47678732+eserhin@users.noreply.github.com>
2019-02-22 05:02:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
+module.exports = function getLoveTrianglesCount(preferences) {
   var triangles = 0;
   for (i = 0; i < preferences.length; i++) {
-  for (j = i; j < preferences.length; j++) {
-    var a = preferences[i];
+	var j=i;
+    var a = preferences[i]-j;
     var b = preferences[a] - j;
     var c = preferences[b] - j;
     if (j == c) {
       triangles++;
-      }
     }
   }
   return triangles;
